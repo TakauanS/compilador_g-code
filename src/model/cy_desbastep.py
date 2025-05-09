0ac8c5b113267722a6fc67be4389b86149f07943
Takauã Silva <takauan9@gmail.com>
2025-05-09 20:01:31 -0300

    Upgrade do cy_desbastep

--- src/model/cy_desbastep.py
+++ src/model/cy_desbastep.py
@@ -1,8 +1,11 @@
+import os
 import textwrap
 from tkinter import messagebox
-from src.model.cycles.cycle_base import CycleBase
+from src.model.cycles.cy_base import CyBase
 
-class CyDesbasteP(CycleBase):
+from src.model.json_handler import JsonHandler
+
+class CyDesbasteP(CyBase):
 
     def __init__(self, di_inicial: float, di_final: float, espessura: float):
 
@@ -27,6 +30,9 @@ class CyDesbasteP(CycleBase):
         self.__di_final = di_final
         self.__espessura = espessura
 
+        self.__json = JsonHandler()
+        self.__json.convert_files()
+
     # Método responsável por armazenar o arquivo main em atributo
     def generate_main(self):
         try:
@@ -37,18 +43,16 @@ class CyDesbasteP(CycleBase):
             N30 R2 = {self.diametro_final}
 
             N40 R3 = -{self.espessura}
-            N50 R4 = -1.6
+            N50 R4 = -{self.get_passe}
 
             N60 _N_CMP_MACVARS_SPF;
 
             N70 M30;''')
 
-            return self.__main
-        
         except Exception as e:
             messagebox.showerror('Compilador G-Code', f'Erro: ao gerar o arquivo main, revise os campos de entrada e tente novamente!\n\n{e}')
-            print(f'Erro: {e}')
-    
+            raise ValueError(e)
+
     # Método responsável por armazenar o arquivo configs em atributo
     def generate_configs(self):
         try:
@@ -58,8 +62,8 @@ class CyDesbasteP(CycleBase):
             N20 G18 G40 G90 G95;
 
             ; Tool and rpm senttings
-            N30 G97 S100;
-            N40 T11D1;
+            N30 G97 S{self.get_rotacao};
+            N40 {self.get_ferramenta};
             N50 M3;
 
             N60 RET;''')
@@ -68,14 +72,14 @@ class CyDesbasteP(CycleBase):
         
         except Exception as e:
             messagebox.showerror('Compilador G-Code', f'Erro: ao gerar o arquivo configs, revise os campos de entrada e tente novamente!\n\n{e}')
-            print(f'Erro: {e}')
+            raise ValueError(e)
 
     # Método responsável por armazenar o arquivo controls em atributo
     def generate_controls(self):
         try:
             self.__controls = textwrap.dedent(f'''
             ; Macro configuration section
-            N10 DEFINE POS_SEG AS G0 G54 X400 Z400; Safety positions macro
+            N10 DEFINE POS_SEG AS G0 {self.get_referencia} X{self.get_posx} Z{self.get_posz}; Safety positions macro
             N20 DEFINE APROX AS G0 X=R1 Z0; X approximation macro
 
             N30 POS_SEG;
@@ -84,7 +88,7 @@ class CyDesbasteP(CycleBase):
             ; Repeating structure section
             FOR R8 = 1 TO R7
                 G91
-                G1 X=R4 F1
+                G1 X=R4 F{self.get_avanco}
                 G1 Z=R3
                 G0 X=R5 Z=ABS(R3)
                 G1 X=R4
@@ -96,7 +100,7 @@ class CyDesbasteP(CycleBase):
         
         except Exception as e:
             messagebox.showerror('Compilador G-Code', f'Erro: ao gerar o arquivo controls, revise os campos de entrada e tente novamente!\n\n{e}')
-            print(f'Erro: {e}')
+            raise ValueError(e)
 
     # Método responsável por armazenar o arquivo macvars em atributo
     def generate_macvars(self):
@@ -123,6 +127,34 @@ class CyDesbasteP(CycleBase):
         
         except Exception as e:
             messagebox.showerror('Compilador G-Code', f'Erro: ao gerar o arquivo macvars, revise os campos de entrada e tente novamente!\n\n{e}')
+            raise ValueError(e)
+
+    # Método responsável por gerar o g-code do ciclo de desbaste parametrizado
+    def generate_gcode(self, name_directory: str):
+        try:
+            self.__imp = self.__json.get_data(self.__json.data_file, 'diretório') # Importa o caminho do diretório que o usuário escolheu
+            self.__directory = f'{self.__imp}/{name_directory}.WPD' # Concatena o nome da pasta com o caminho do diretório
+
+            if os.path.exists(self.__directory):
+                messagebox.showerror('Compilador G-Code', f'Erro na geração do g-code, pois o nome de projeto: {name_directory}, já é existente!')
+            else:
+                os.mkdir(self.__directory)
+                print(f' - O projeto: {name_directory}, foi criado com sucesso!')
+
+                with open(f'{self.__directory}/CMP_MAIN.mpf', 'w') as f:
+                    f.write(self.__main)
+
+                with open(f'{self.__directory}/CMP_CONFIGS.spf', 'w') as f:
+                    f.write(self.__configs)
+
+                with open(f'{self.__directory}/CMP_CONTROLS.spf', 'w') as f:
+                    f.write(self.__controls)
+
+                with open(f'{self.__directory}/CMP_MACVARS.spf', 'w') as f:
+                    f.write(self.__macvars)
+            
+        except Exception as e:
+            messagebox.showerror('Compilador G-Code', f'Erro na geração do g-code, verifique o seguinte problema:\n\n{e}')
             print(f'Erro: {e}')
 
     @property
