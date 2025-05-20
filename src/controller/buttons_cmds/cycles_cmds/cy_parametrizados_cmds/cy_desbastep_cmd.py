6769b2c3fd9204e53e77895e10de6a43a4818dc3
Takauã Silva <takauan9@gmail.com>
2025-05-20 16:33:01 -0300

    Correção de exceções no método de gerar o g-code do ciclo de desbaste parametrizado

--- src/controller/buttons_cmds/cycles_cmds/cy_parametrizados_cmds/cy_desbastep_cmd.py
+++ src/controller/buttons_cmds/cycles_cmds/cy_parametrizados_cmds/cy_desbastep_cmd.py
@@ -30,19 +30,22 @@ class DesbastePCmds:
             self._dif = float(self.__master.entry_dif.get()) # Conversão do campo de entry - diâmetro final
             self._esp = float(self.__master.entry_esp.get()) # Conversão do campo de entry - espessura
 
-            self._ip = self.__assents.criar_inputdialog('Compilador G-Code', DesbastePCmds.txt)
+            self._ip = self.__assents.criar_inputdialog('Compilador G-Code', DesbastePCmds.txt) # Abre a tela de inputdialog para informar o nome do projeto/ciclo
+            self._name_project = self._ip.get_input() # Retorna o nome do projeto/ciclo
 
-            self.__desbastep = CyDesbasteP(self._dii, self._dif, self._esp)
+            if self._name_project is None or self._name_project == '':
+                messagebox.showerror('Compilador G-Code', 'Ocorreu um erro ao tentar salvar o seu ciclo nos arquivos.')
+                return
+            else:
+                self.__desbastep = CyDesbasteP(self._dii, self._dif, self._esp)
+                self.__desbastep.rotacao(100.0)
 
-            self.__desbastep.rotacao(100.0)
+                self.__desbastep.initialize_files()
+                self.__desbastep.generate_gcode(self._name_project)
 
-            self.__desbastep.initialize_files()
-            self.__desbastep.generate_gcode(self._ip.get_input())
+                messagebox.showinfo('Compilador G-Code', f'Ciclo de desbaste parametrizado gerado com sucesso e salvo nos seus arquivos.')
+                print(' - Ciclo de desbaste parametrizado gerado com sucesso e salvo nos seus arquivos.')
 
         except Exception as e:
             messagebox.showerror('Compilador G-Code', 'Erro na geração do g-code, revise os campos e tente novamente!')
             print(f'Erro na geração do g-code final:\n\n{e}')
-        
-        else:
-            messagebox.showinfo('Compilador G-Code', f'Ciclo de desbaste parametrizado gerado com sucesso e salvo nos seus arquivos.')
-            print(' - Ciclo de desbaste parametrizado gerado com sucesso e salvo nos seus arquivos.')
