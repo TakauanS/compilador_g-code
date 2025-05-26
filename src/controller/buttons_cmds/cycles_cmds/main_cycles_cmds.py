496f9a5d0d537d217532891bc05d66a2c582406e
Takauã Silva <takauan9@gmail.com>
2025-05-26 10:35:51 -0300

    Implementação de método de limpar as telas

--- src/controller/buttons_cmds/cycles_cmds/main_cycles_cmds.py
+++ src/controller/buttons_cmds/cycles_cmds/main_cycles_cmds.py
@@ -1,16 +1,19 @@
 from tkinter import messagebox
 
+from src.controller.buttons_cmds.main_screen_cmds import MainScreenCmds
 from src.view.view_cycles.cy_parametrizados.view_cy_desbastep import ViewCyDesbasteP
 
 class MainCyclesCmds:
 
-    def __init__(self, mtr):
-        self.__mtr = mtr
+    def __init__(self, master):
+        self.__master = master
+        self.__clean = MainScreenCmds(self.__master) # Instância de classe para chamar método para limpar a tela
 
     # Método responsável por fazer a chamada do ciclo de desbaste parametrizado
     def call_desbastep(self):
         try:
-            self.__desbastep = ViewCyDesbasteP(self.__mtr)
+            self.__clean.clean_screen()
+            self.__desbastep = ViewCyDesbasteP(self.__master)
             self.__desbastep.mainloop()
         
         except Exception as e:
--- src/controller/buttons_cmds/main_screen_cmds.py
+++ src/controller/buttons_cmds/main_screen_cmds.py
@@ -10,9 +10,21 @@ class MainScreenCmds:
     def call_maincycles(self):
         try:
             from src.view.view_cycles.view_main_cycles import ViewMainCycles  
-               
+
+            self.clean_screen()
             self.main_cycles = ViewMainCycles(self.__master)
 
         except Exception as e:
             messagebox.showerror('Compilador G-Code', f'Ocorreu um erro ao chamar a tela de ciclos de usinagem:\n\n{e}')
             print(f'Erro: {e}')
+
+    # Método responsável por limpar a tela principal
+    def clean_screen(self):
+        try:
+            self.__lista_widgets = self.__master.fra_main.winfo_children()
+
+            for widget in self.__lista_widgets:
+                widget.place_forget()
+        
+        except Exception as e:
+            raise ValueError(f'Erro no momento de limpar a tela principal: {e}')
